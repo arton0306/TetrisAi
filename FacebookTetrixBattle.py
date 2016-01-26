a2b643b7065159de1bce2c6147832681506c06d0
Arton Chang <arton0306@gmail.com>
2016-01-26 23:01:04 +0800

    better, but still not all correct

--- FacebookTetrixBattle.py
+++ FacebookTetrixBattle.py
@@ -16,7 +16,7 @@ class FacebookTetrixBattle:
     hDelta["J"] = ( 3, 4, 3, 3 )
     hDelta["L"] = ( 3, 4, 3, 3 )
 
-    sKeyBoardSimulator = CPKeyBoardSimulator(pressKeySec = 0.06, gap2KeySec = 0.3)
+    sKeyBoardSimulator = CPKeyBoardSimulator(pressKeySec = 0.05, gap2KeySec = 0.2)
 
     def __init__( self ):
         pass
