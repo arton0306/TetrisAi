5c1fd6895d2f7a3b080e924431563773c576f4be
Arton Chang <arton0306@gmail.com>
2016-01-26 08:40:46 +0800

    add cross platform keyboard sender

---
+++ CPKeyBoardSimulator.py
@@ -0,0 +1,90 @@
+# CP: cross platform 
+# using pyautogui lib to implement keyboard sender
+
+import pyautogui
+import time
+
+# screenWidth, screenHeight = pyautogui.size()
+# currentMouseX, currentMouseY = pyautogui.position()
+# pyautogui.typewrite('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
+# pyautogui.keyDown('shift')
+# pyautogui.typewrite('under shift...', interval=0.25)
+# pyautogui.typewrite(['left', 'left', 'left', 'left', 'left', 'left'])
+# pyautogui.keyUp('shift')
+# pyautogui.keyDown('a')
+# time.sleep( 0.13 )
+# pyautogui.keyUp('a')
+
+class CPKeyBoardSimulator:
+    def __init__( self ):
+        self._keyPressedSec = 0.13
+        self._between2KeySec = 0.11
+
+    def SendKey( self, aKey ):
+        self.printKey( aKey )
+        pyautogui.keyDown(aKey)
+        time.sleep( self._keyPressedSec )
+        pyautogui.keyUp(aKey)
+        time.sleep( self._between2KeySec )
+
+    def printKey( self, aKey ):
+        keyList = ['x', 'z', 'c', 'space', 'right', 'left']
+        keyName = [ "X", "Z", "C", "Space", "Right", "Left" ]
+        print(keyName[keyList.index(aKey)])
+
+    def RotateRight( self ):
+        self.SendKey('x')
+
+    def RotateLeft( self ):
+        self.SendKey('z')
+
+    def HoldBlock( self ):
+        self.SendKey('c')
+
+    def FallInstantly( self ):
+        self.SendKey('space')
+
+    def MoveRight( self ):
+        self.SendKey('right')
+
+    def MoveLeft( self ):
+        self.SendKey('left')
+
+    def MoveLeftMulti( self, count ):
+        for i in range( count ):
+            self.MoveLeft()
+
+    def MoveRightMulti( self, count ):
+        for i in range( count ):
+            self.MoveRight()
+
+    def RotateRightMulti( self, count ):
+        if count < 3:
+            for i in range( count ):
+                self.RotateRight()
+        else: # count == 3
+            self.RotateLeft()
+
+if __name__ == '__main__':
+    time.sleep(3)
+    """
+    "X CZC"
+    keyBoard = KeyBoardSimulator()
+    keyBoard.RotateRight()
+    keyBoard.SendKey( 0x20 )
+    keyBoard.RotateLeft()
+    keyBoard.MoveLeft()
+    keyBoard.HoldBlock()
+    keyBoard.MoveRight()
+    keyBoard.HoldBlock()
+    """
+
+    keyBoard = CPKeyBoardSimulator()
+    cmdOrder = [keyBoard.MoveLeft, keyBoard.MoveLeft, keyBoard.MoveLeft, keyBoard.FallInstantly]
+    #cmdOrder = [ keyBoard.FallInstantly ]
+    for i in range(20):
+        keyBoard.MoveLeftMulti(3)
+        keyBoard.RotateRightMulti(2)
+        keyBoard.MoveRightMulti(2)
+        keyBoard.FallInstantly()
+
